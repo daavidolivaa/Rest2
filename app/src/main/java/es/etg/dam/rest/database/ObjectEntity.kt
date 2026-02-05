c7ff2035711ad85d36645e8edf719f5aef6d8fa7
daavidolivaa <deividolivahuelamo@gmail.com>
2026-02-05 19:55:17 +0100

    Final commit

--- app/src/main/java/es/etg/dam/rest/database/ObjectEntity.kt
+++ app/src/main/java/es/etg/dam/rest/database/ObjectEntity.kt
@@ -1,11 +1,12 @@
 package es.etg.dam.rest.database
 
+import android.R
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "objects")
 data class ObjectEntity(
-    @PrimaryKey val id: String,
+    @PrimaryKey val id: Int,
     val name: String,
     val data: String
 )
--- app/src/main/java/es/etg/dam/rest/model/ObjectModel.kt
+++ app/src/main/java/es/etg/dam/rest/model/ObjectModel.kt
@@ -1,7 +1,9 @@
 package es.etg.dam.rest.model
 
+import android.R
+
 data class ObjectModel(
-    val id: String,
+    val id: Int,
     val name: String,
     val data: Map<String, Any>?
 )
